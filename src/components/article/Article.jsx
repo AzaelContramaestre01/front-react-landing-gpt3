fe8f036bc8b5a186847c0a43abf61bd309f041a8
azazelcontramaestre01 <galaxyhhd@hotmail.com>
2023-02-28 23:09:01 -0300

    add(blog):Blog component added to the page

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,23 @@
 import React from "react"
+
 import './article.css'
 
-const Article = () => {
-  return <div>Article</div>
+const Article = ({imgUrl, date, title}) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+        <img src={imgUrl} alt="Blog Image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  )
 }
 
 export default Article
